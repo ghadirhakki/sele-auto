5367c403371811238ee2a6236e00b15bc63328ed
Ghadir <99799324+ghadirhakki@users.noreply.github.com>
2024-06-11 11:10:11 +0100

    Update test-auto.py

--- test-auto.py
+++ test-auto.py
@@ -106,7 +106,7 @@ try:
     logger.info("menu opened screenshot")
 
     #click on mes tournées 
-    tours = driver.find_element(By.XPATH, /html/body/div/div[2]/div/div/div[1]/div/ul/li[1]) 
+    tours = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[1]/div/ul/li[1]") 
     tours.click
     logger.info("sous_menu_tours opened")
 
